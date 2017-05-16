a15bfb146e93b84325988cb2c0e3487db0d9936f
luismachado <luismachado5@gmail.com>
2017-05-16 14:13:58 +0100

    Initial Commit

---
+++ Running Pace Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Running Pace Calculator
+//
+//  Created by Luís Machado on 16/05/17.
+//  Copyright © 2017 LuisMachado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
